ac4498048bb3552606388a87c38dcca07a3be83a
unknown <kvainumaa@gmail.com>
2022-09-25 12:24:58 +0300

    add week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿//for-loop
+//DRY-principle - Do Not Repeat yourself
+
+int i = 0;
+
+for(i = 0; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We dont need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");
